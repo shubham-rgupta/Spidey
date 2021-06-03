a739448f99f5e22fe38ae97b1a3175d7427dc68b
shubham-rgupta <71450289+shubham-rgupta@users.noreply.github.com>
2021-06-03 19:22:20 +0530

    Initial commit

---
+++ Assets/Scripts/ConstraintController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstraintController : MonoBehaviour
+{
+    Vector3 originalPos;
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    
+    void Update()
+    {
+        transform.position = originalPos;
+    }
+}
---
+++ Assets/Scripts/grounder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grounder : MonoBehaviour
+{
+    int layermask;
+    void Start()
+    {
+        layermask = LayerMask.GetMask("Ground");
+    }
+
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position+ new Vector3(0f,0.75f,0f),-transform.up, out hit,Mathf.Infinity, layermask))
+        {
+            transform.position = hit.point + new Vector3(0f, 0.75f, 0f);
+        }
+    }
+}
