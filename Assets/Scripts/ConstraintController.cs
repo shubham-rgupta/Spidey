900597482917dddee8c1c386c18b3b00b539e8b8
shubham-rgupta <71450289+shubham-rgupta@users.noreply.github.com>
2021-06-04 15:12:28 +0530

    initial

--- Assets/Scripts/ConstraintController.cs
+++ Assets/Scripts/ConstraintController.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ConstraintController : MonoBehaviour
 {
     Vector3 originalPos;
+    public GameObject moveCube;
+    float legMoveSpeed= 7f;
+    public ConstraintController oppLegs;
+    private bool isMoving = false;
+    private bool moving = false;
+    public float moveDistance = 0.7f;
+    public float stopDistance = 0.5f;
+
     void Start()
     {
         originalPos = transform.position;
@@ -13,6 +21,27 @@ public class ConstraintController : MonoBehaviour
     
     void Update()
     {
-        transform.position = originalPos;
+        float distanceToMoveCube = Vector3.Distance(transform.position, moveCube.transform.position);
+        if((distanceToMoveCube >= 0.7f && !oppLegs.isItMoving()) || moving)
+        {
+            moving = true;
+            transform.position = Vector3.Lerp(transform.position, moveCube.transform.position + new Vector3(0f, 0.3f, 0f), Time.deltaTime * legMoveSpeed);
+            originalPos = transform.position;
+            isMoving = true;
+            if (distanceToMoveCube < stopDistance)
+            {
+                moving = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, originalPos + new Vector3(0f,-0.3f, 0f), Time.deltaTime * legMoveSpeed*3);
+            isMoving = false;
+        }
+    }
+
+    public bool isItMoving()
+    {
+        return isMoving;
     }
 }
--- Assets/Scripts/grounder.cs
+++ Assets/Scripts/grounder.cs
@@ -5,18 +5,56 @@ using UnityEngine;
 public class grounder : MonoBehaviour
 {
     int layermask;
+    GameObject raycastOrigin;
+
     void Start()
     {
         layermask = LayerMask.GetMask("Ground");
+        raycastOrigin = transform.parent.gameObject;
     }
 
     
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position+ new Vector3(0f,0.75f,0f),-transform.up, out hit,Mathf.Infinity, layermask))
+        if(Physics.Raycast(raycastOrigin.transform.position+ new Vector3(0f,0.75f,0f),-transform.up, out hit,Mathf.Infinity, layermask))
         {
             transform.position = hit.point + new Vector3(0f, 0.75f, 0f);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
